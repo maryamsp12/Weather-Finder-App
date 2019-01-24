ca267e52521e8c59aec19bbca47054918972b9d9
maryamsp12 <maryamsp12@example.com>
2019-01-24 21:39:20 +0200

    first commit

---
+++ src/components/title.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Title = () => {
+    return ( 
+        <div>
+            <h1  className="title-container__title">Weather Finder</h1>
+            </div>
+     );
+}
+ 
+export default Title;
