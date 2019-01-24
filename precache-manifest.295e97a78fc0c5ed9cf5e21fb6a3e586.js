e13631eb1293a24fdaaf3f3641f51236d1ea7898
maryamsp12 <maryamsp12@example.com>
2019-01-24 22:15:17 +0200

    Updates

---
+++ precache-manifest.295e97a78fc0c5ed9cf5e21fb6a3e586.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/Weather-Finder-App/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "0730298aa473e2b58248",
+    "url": "/Weather-Finder-App/static/css/main.29b3c8de.chunk.css"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/Weather-Finder-App/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "abe8b2cbdd4408d2a07c",
+    "url": "/Weather-Finder-App/static/js/1.abe8b2cb.chunk.js"
+  },
+  {
+    "revision": "6cda25a5415805f3bf6c",
+    "url": "/Weather-Finder-App/static/js/runtime~main.6cda25a5.js"
+  },
+  {
+    "revision": "0730298aa473e2b58248",
+    "url": "/Weather-Finder-App/static/js/main.0730298a.chunk.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/Weather-Finder-App/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/Weather-Finder-App/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/Weather-Finder-App/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "abe8b2cbdd4408d2a07c",
+    "url": "/Weather-Finder-App/static/css/1.4c581821.chunk.css"
+  },
+  {
+    "revision": "aa685ff512a949f3127ed14a29677d14",
+    "url": "/Weather-Finder-App/index.html"
+  }
+];
